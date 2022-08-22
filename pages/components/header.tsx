cbe37dc794cf65802481ca733909dc81ac3973d9
tosaken1116 <nagato20021122@gmail.com>
2022-08-22 14:31:19 +0900

    #6[add]トップページの作成

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div className=" text-shadow  text-3xl font-bold  text-[#336633] shadow-black ">
         Myfont
       </div>
-      <div className="border-t-2 bg-gradient-to-r from-[#ccee00] to-[#caee0042]"></div>
+      <div className="hover border-t-2 bg-gradient-to-r from-[#ccee00] to-[#caee0042]"></div>
     </div>
   );
 }
